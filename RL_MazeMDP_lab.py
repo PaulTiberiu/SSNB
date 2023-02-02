9c1874331409bce66fa91e9f134ae2de7671b469
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-02-02 17:39:38 +0100

    Add files via upload

---
+++ RL_MazeMDP_lab.py
@@ -0,0 +1,245 @@
+import os
+from typing import Tuple, List
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
+
+from mazemdp.toolbox import egreedy, egreedy_loc, softmax, sample_categorical
+from mazemdp.maze_plotter import show_videos
+from mazemdp.mdp import Mdp
+from bbrl_gym.envs.maze_mdp import MazeMDPEnv
+
+# For visualization
+os.environ["VIDEO_FPS"] = "5"
+if not os.path.isdir("./videos"):
+    os.mkdir("./videos")
+
+from IPython.display import Video
+
+# Settings
+NB_EPISODES = 50
+TIMEOUT = 25
+
+import gym
+import bbrl_gym
+
+
+env = gym.make("MazeMDP-v0", kwargs={"width": 5, "height": 5, "ratio": 0.2})
+env.reset()
+
+# in dynamic programming, there is no agent moving in the environment
+env.init_draw("The maze")
+
+from mazemdp import random_policy
+
+def temporal_difference(
+    mdp: MazeMDPEnv,
+    policy: np.ndarray,
+    nb_episodes: int = 50,
+    alpha: float = 0.2,
+    timeout: int = 25,
+    render: bool = True,
+) -> np.ndarray:
+    # alpha: learning rate
+    # timeout: timeout of an episode (maximum number of timesteps)
+    v = np.zeros(mdp.nb_states)  # initial state value v
+    mdp.timeout = timeout
+
+    if render:
+        mdp.init_draw("Temporal differences")
+
+    for _ in tqdm(range(nb_episodes)):  # for each episode
+
+        # Draw an initial state randomly (if uniform is set to False, the state is drawn according to the P0
+        #                                 distribution)
+        x = mdp.reset(uniform=True)
+        done = False
+        while not done:  # update episode at each timestep
+            # Show agent
+            if render:
+                mdp.draw_v_pi(v, policy)
+
+            # Step forward following the MDP: x=current state,
+            #                                 pol[i]=agent's action according to policy pol,
+            #                                 r=reward gained after taking action pol[i],
+            #                                 done=tells whether the episode ended,
+            #                                 and info gives some info about the process
+            [y, r, done, _] = mdp.step(egreedy_loc(policy[x], mdp.action_space.n, epsilon=0.2))
+
+            # To be completed...
+            delta = r+mdp.gamma*v[x+1]-v[x]
+            v[x] = v[x]+alpha*delta
+
+            # Update agent's position (state)
+            x = y
+
+    if render:
+        # Show the final policy
+        mdp.current_state = 0
+        mdp.draw_v_pi(v, policy, title="Temporal Differences")
+    return v
+
+# Note that using a random policy, the chance to get an interesting value function is low
+# Instead, you may get the value of an optimal policy obtained from a dynamic programming method
+policy = random_policy(env)
+v = temporal_difference(env, policy)
+
+# ------------------ Value Iteration with the Q function ---------------------#
+# Given a MDP, this algorithm computes the optimal action value function Q
+# It then derives the optimal policy based on this function
+def value_iteration_q(mdp: MazeMDPEnv, render: bool = True) -> Tuple[np.ndarray, List[float]]:
+    q = np.zeros((mdp.nb_states, mdp.action_space.n))  # initial action values are set to 0
+    q_list = []
+    stop = False
+
+    if render:
+        mdp.init_draw("Value iteration Q")
+
+    while not stop:
+        qold = q.copy()
+
+        if render:
+            mdp.draw_v(q, title="Value iteration Q")
+
+        for x in range(mdp.nb_states):
+            if x not in mdp.terminal_states:
+                for u in range(mdp.action_space.n):
+                    summ = 0
+                    for y in range(mdp.nb_states):
+                        summ += mdp.P[x, u, y] * np.max(qold[y, :])
+                    q[x,u]=(mdp.r[x,u] + mdp.gamma * summ) #modification!!!!!!!!!!!!!!!!!!!!!!!
+        
+        if (np.linalg.norm(q - qold)) <= 0.01:
+            stop = True
+        q_list.append(np.linalg.norm(q))
+
+    if render:
+        mdp.draw_v(q, title="Value iteration Q")
+        #mdp.mdp.plotter.video_writer.release() essaye sur google collab
+
+    return q, q_list
+
+def get_policy_from_q(q: np.ndarray) -> np.ndarray:
+    # Outputs a policy given the action values
+    return np.argmax(q,axis=1)
+
+q, _ = value_iteration_q(env)
+policy = get_policy_from_q(q)
+v = temporal_difference(env, policy)
+
+def get_policy_from_q(q: np.ndarray) -> np.ndarray:
+    # Outputs a policy given the action values
+    return np.argmax(q,axis=1)
+
+# --------------------------- Q-Learning epsilon-greedy version -------------------------------#
+
+# Given an exploration rate epsilon, the QLearning algorithm computes the state action-value function
+# based on an epsilon-greedy policy
+# alpha is the learning rate
+
+
+def q_learning_eps(
+    mdp: MazeMDPEnv,
+    epsilon: float,
+    nb_episodes: int = 20,
+    timeout: int = 50,
+    alpha: float = 0.5,
+    render: bool = True,
+) -> Tuple[np.ndarray, List[float]]:
+    # Initialize the state-action value function
+    # alpha is the learning rate
+    q = np.zeros((mdp.nb_states, mdp.action_space.n))
+    q_list = []
+
+    # Run learning cycle
+    mdp.timeout = timeout  # episode length
+
+    if render:
+        mdp.init_draw("Q-learning e-greedy")
+
+    for _ in tqdm(range(nb_episodes)):
+        # Draw the first state of episode i using a uniform distribution over all the states
+        x = mdp.reset(uniform=True)
+        done = False
+        while not done:
+            if render:
+                # Show the agent in the maze
+                mdp.draw_v_pi(q, q.argmax(axis=1))
+
+            # Draw an action using an epsilon-greedy policy
+            u = egreedy(q, x, epsilon)
+
+            # Perform a step of the MDP
+            [y, r, done, _] = mdp.step(u)
+
+            # To be completed
+            delta = r+mdp.gamma*np.argmax(q[x+1,u]-q[x,u])
+            q[x,u] = q[x,u]+alpha*delta
+
+            # Update the agent position
+            x = y
+        q_list.append(np.linalg.norm(q))
+
+    if render:
+        # Show the final policy
+        mdp.current_state = 0
+        mdp.draw_v_pi(q, get_policy_from_q(q), title="Q-learning e-greedy")
+    return q, q_list
+
+epsilon = 0.02
+q, q_list = q_learning_eps(env, epsilon)
+
+# --------------------------- Q-Learning softmax version ----------------------------#
+
+# Given a temperature "tau", the QLearning algorithm computes the state action-value function
+# based on a softmax policy
+# alpha is the learning rate
+
+
+def q_learning_soft(
+    mdp: MazeMDPEnv,
+    tau: float,
+    nb_episodes: int = 20,
+    timeout: int = 50,
+    alpha: float = 0.5,
+    render: bool = True,
+) -> Tuple[np.ndarray, List[float]]:
+    # Initialize the state-action value function
+    # alpha is the learning rate
+    q = np.zeros((mdp.nb_states, mdp.action_space.n))
+    q_list = []
+
+    # Run learning cycle
+    mdp.timeout = timeout  # episode length
+
+    if render:
+        mdp.init_draw("Q-learning softmax")
+
+    for _ in tqdm(range(nb_episodes)):
+        # Draw the first state of episode i using a uniform distribution over all the states
+        x = mdp.reset(uniform=True)
+        done = False
+        while not done:
+            if render:
+                # Show the agent in the maze
+                mdp.draw_v_pi(q, q.argmax(axis=1))
+
+            # To be completed 
+            u = softmax(q, x, tau)# (here, call the softmax function and sample from it)
+            q = sample_categorical(u)
+            [y,r,done] = mdp.step(u)
+
+            # To be completed
+            delta = r+mdp.gamma*np.argmax(q[x+1,u]-q[x,u])
+            q[x,u] = q[x,u]+alpha*delta
+
+            x = y
+        q_list.append(np.linalg.norm(q))
+
+    if render:
+        # Show the final policy
+        mdp.current_state = 0
+        mdp.draw_v_pi(q, get_policy_from_q(q), title="Q-learning softmax")
+    return q, q_list
+
