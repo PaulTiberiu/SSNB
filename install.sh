1e6626f6f8ddc8415c5b897f24c00ffd7fe629eb
Ethan2541 <eluong41@gmail.com>
2023-02-23 23:16:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@ while true; do
     read -p "Do you wish to proceed with the installation of SSNB ? [y/n] " answer
     case $answer in
         [Yy]* ) make install; break;;
-        [Nn]* ) echo "Installation aborted\n"; exit;;
+        [Nn]* ) echo "Installation aborted"; exit;;
         * ) echo "[y/n] ";;
     esac
 done
 
-echo "=== Installing setup tools ===\n"
+echo "=== Installing setup tools ==="
 sudo apt-get install curl
 sudo apt-get install git
 sudo apt-get install python3-pip
 
 ssnb_directory=$(pwd)
 
-echo "=== Installing bbrl ===\n"
+echo "=== Installing bbrl ==="
 git clone https://github.com/osigaud/bbrl
 cd bbrl/
 sudo pip install -e .
@@ -24,12 +24,12 @@ sudo pip install hydra-core --upgrade --pre
 
 cd ../
 
-echo "=== Installing dependencies for MuJoCo ===\n"
+echo "=== Installing dependencies for MuJoCo ==="
 sudo apt-get install libglew-dev
 sudo apt-get install libosmesa6-dev
 sudo apt-get install patchelf
 
-echo "=== Installing MuJoCo ===\n"
+echo "=== Installing MuJoCo ==="
 curl -o mujoco210-linux-x86_64.tar.gz https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
 tar -xvf mujoco210-linux-x86_64.tar.gz -C ~/
 rm -rf mujoco210-linux-x86_64.tar.gz
@@ -43,8 +43,8 @@ echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$home_directory/.mujoco/mujoco210
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib/nvidia" >> ~/.bashrc
 echo "export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so" >> ~/.bashrc
 
-echo "=== Installing SSNB ===\n"
+echo "=== Installing SSNB ==="
 cd $ssnb_directory
 sudo pip install -e .
 
-echo "Installation successful\n"
+echo "Installation successful"
