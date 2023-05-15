99e5166949e66e5128137a3c36de8573da36dc75
Ethan Luong <eluong41@gmail.com>
2023-05-15 18:14:45 +0200

    Small fix for ddpg

--- ssnb/algos/ddpg.py
+++ ssnb/algos/ddpg.py
@@ -133,7 +133,7 @@ class DDPG:
                 if epoch > 0:
                     train_workspace.zero_grad()
                     train_workspace.copy_n_last_steps(1)
-                    self.agent['train_agent'](train_workspace, t=1, n_steps=self.cfg.algorithm.n_steps - 1)
+                    self.agent['train_agent'](train_workspace, t=1, n_steps=self.cfg.algorithm.n_steps)
 
                 else:
                     self.agent['train_agent'](train_workspace, t=0, n_steps=self.cfg.algorithm.n_steps)
--- ssnb/algos/td3.py
+++ ssnb/algos/td3.py
@@ -136,7 +136,7 @@ class TD3:
                 if epoch > 0:
                     train_workspace.zero_grad()
                     train_workspace.copy_n_last_steps(1)
-                    self.agent['train_agent'](train_workspace, t=1, n_steps=self.cfg.algorithm.n_steps)  # check if it should be n_steps=cfg.algorithm.n_steps - 1
+                    self.agent['train_agent'](train_workspace, t=1, n_steps=self.cfg.algorithm.n_steps)
 
                 else:
                     self.agent['train_agent'](train_workspace, t=0, n_steps=self.cfg.algorithm.n_steps)
